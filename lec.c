28eaf9af4e6c442a499fa5b00d6d1e5bf857e98c
chandanverma03 <114793891+chandanverma03@users.noreply.github.com>
2023-02-18 23:32:45 +0530

    Add files via upload

---
+++ lec.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("Enter first number : ");
+	scanf("%d",&a);
+	printf("Enter second number : ");
+	scanf("%d",&b);
+	c = a+b;
+	printf("Sum of two number is %d",c);
+//	return 0;
+}

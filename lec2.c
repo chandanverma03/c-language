c299b4bca018fb8015f65964031307bafe223ea8
chandanverma03 <114793891+chandanverma03@users.noreply.github.com>
2023-02-18 23:33:16 +0530

    Add files via upload

---
+++ lec2.c
@@ -0,0 +1,55 @@
+/*#include<stdio.h>
+int main()
+{
+	int x = 15;
+	float pi = 3.14;
+	float a = 2*pi*x;
+	printf("Circumfrance of the circle is : %f",a);
+	return 0;
+}*/
+//------------------------------------------------------------------------------------
+//swap the numbers 
+/*#include<stdio.h>
+int main()
+{
+	int a = 10, b = 8, c;
+	c = b;
+	b = a;
+	a = c;
+	printf("a = %d\n",a);
+	printf("b = %d\n",b);
+	return 0;
+}*/
+//-----------------------------------------------------------------------------------
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	int b = sizeof(a);
+//	printf("%d",sizeof(a));
+//	return 0;
+//}
+//---------------------------------------------------------------------------------------
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	int n = sizeof(a);
+//	float b = 3.14;
+//	int c = (int)b;
+//	printf("%d \n",n);
+//	printf("%d",c);
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+

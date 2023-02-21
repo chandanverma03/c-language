0a74bd0e56d525a7f53b1ce28e9788c07fd75a5f
chandanverma03 <114793891+chandanverma03@users.noreply.github.com>
2023-02-21 21:47:16 +0530

    Add files via upload

---
+++ lecture4.c
@@ -0,0 +1,32 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int a = (1,2,3,4,5);
+//	printf("%d",a);        //value of a is always goes with the last value in the bracket(ans is 5)
+//	return 0;
+//}
+
+
+//first question of hackerrank :=>
+//#include<stdio.h>
+//int main()
+//{
+//	char s[100];
+//	scanf("%[^\n]%*c",&s);
+//	printf("Hello, World!\n");
+//	printf("%s",s);
+//	return 0;
+//		
+//}
+
+
+//second question of hackerrank :=>
+//#include<stdio.h>
+//#include<string.h>
+//void main()
+//{
+//	char ch,s[100],sen[100];
+//	scanf("%c\n%s\n%[^\n]%*c",&ch,&s,&sen);
+//	printf("%c\n%s\n%s",ch,s,sen);
+//}
+//
